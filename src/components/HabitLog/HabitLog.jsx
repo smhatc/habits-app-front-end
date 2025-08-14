7960f10ba54ff02cea11261df1d62bac78f7831d
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2025-08-14 09:26:45 +0300

    Added HabitLog styles

--- src/components/HabitLog/HabitLog.jsx
+++ src/components/HabitLog/HabitLog.jsx
@@ -1,14 +1,24 @@
+import "./HabitLog.css";
+
 const HabitLog = ({ habit }) => {
   return (
-    <article>
-      <h2>Activity History</h2>
-      {habit.habitLog.map((log) => (
-        <div key={log._id}>
-          <img src="/assets/images/checkmark.png" alt="Checkmark Icon" />
-          <p>Completed on {new Date(log.createdAt).toLocaleDateString()}</p>
-        </div>
-      ))}
-    </article>
+    <>
+      <h2 className="habitlog-header">Activity History</h2>
+      <article className="habitlog-container">
+        {habit.habitLog.map((log) => (
+          <div className="habitlog-log" key={log._id}>
+            <img
+              className="habitlog-checkmark"
+              src="/assets/images/checkmark.png"
+              alt="Checkmark Icon"
+            />
+            <p className="habitlog-date">
+              Completed on {new Date(log.createdAt).toLocaleDateString()}
+            </p>
+          </div>
+        ))}
+      </article>
+    </>
   );
 };
 
